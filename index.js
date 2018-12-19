a256a2a811dedefaf1b7ad0173b5ffb11b75445b
pierreneter <pierreneter@gmail.com>
2018-12-19 14:51:58 +0700

    update way to catch new versions

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
 const fs = require('fs');
+const path = require('path');
 const r2 = require('r2');
 const trash = require('trash');
 const Git = require('nodegit');
 const copydir = require('copy-dir');
 const npminit = require('npm');
+const { xor } = require('lodash');
+const semver = require('semver');
 const packageJson = require('./package.json');
 
+const registry = 'https://registry.npmjs.org';
+let currentPosition = 0;
+let tagLength = 0;
+let distTags = [];
+
 (async () => {
-  const data = await r2('https://api.github.com/repos/twbs/bootstrap/releases/latest', {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_TOKEN}`,
-    },
-  }).json;
+  const originRawRes = await r2('https://registry.npmjs.org/bootstrap').response;
+  const originRes = await originRawRes.json();
+  const rawOriginReleases = originRes.versions;
+  const originReleasesTagName = Object.keys(rawOriginReleases).map(release => `v${release}`);
+  distTags = originRes['dist-tags'];
 
-  const tagName = data.tag_name;
-  if (!tagName) {
-    console.error('Can\'t get tag name of bootstrap');
-    process.exit(1);
-  }
-  const currentVersion = tagName.substr(1);
-  console.log(`Current bootstrap version: ${currentVersion}`);
+  const nojsRes = await r2('https://registry.npmjs.org/bootstrap-nojs').json;
+  const rawNojsReleases = nojsRes.versions;
+  const nojsReleasesTagName = Object.keys(rawNojsReleases).map(release => `v${release}`);
 
-  const packageOnNPM = await r2('https://registry.npmjs.org/bootstrap-nojs').json;
+  const needRelease = xor(originReleasesTagName, nojsReleasesTagName, [
+    'v3.3.7',
+    'v3.3.6',
+    'v4.1.3',
+    'v4.1.2',
+    'v4.1.1',
+    'v4.1.0',
+    'v4.0.0',
+    'v4.0.0-beta.3',
+    'v4.0.0-beta.2',
+    'v4.0.0-beta',
+    'v4.0.0-alpha.6',
+    'v4.0.0-alpha.5',
+    'v4.0.0-alpha.4',
+    'v4.0.0-alpha.3',
+    'v4.0.0-alpha.2',
+  ]).filter(version => semver.gt(version, '3.3.5'));
+  tagLength = needRelease.length;
 
-  const currentNoJSVersion = packageOnNPM['dist-tags'].latest;
-  console.log(`Current bootstrap-nojs version: ${currentNoJSVersion}`);
+  if (!needRelease.length) return;
 
-  if (currentVersion === currentNoJSVersion) {
-    console.error('This version is published');
-    process.exit(0);
-  }
+  if (fs.existsSync('./bootstrap')) { await trash(['./bootstrap']); }
+  await Git.Clone.clone('https://github.com/twbs/bootstrap', 'bootstrap');
+  const repo = await Git.Repository.open(path.resolve(__dirname, 'bootstrap'));
 
-  if (fs.existsSync('./bootstrap')) {
-    await trash(['./bootstrap']);
-  }
+  checkout(repo, needRelease, tagLength);
+  await trash(['bootstrap*', '!bootstrap']);
+})();
 
-  Git.Clone('https://github.com/twbs/bootstrap', 'bootstrap').then(() => {
+const checkout = (repo, versions, tagLength) => {
+  const version = versions[currentPosition];
+  let commit = null;
+  Git.Reference.dwim(repo, `refs/tags/${version}`).then(function (ref) {
+    return ref.peel(Git.Object.TYPE.COMMIT);
+  }).then(function (ref) {
+    return repo.getCommit(ref);
+  }).then((rawCommit) => {
+    commit = rawCommit;
+    return Git.Checkout.tree(repo, commit, { checkoutStrategy: Git.Checkout.STRATEGY.SAFE });
+  }).then(() => {
+    return repo.setHeadDetached(commit, repo.defaultSignature, "Checkout: HEAD " + commit.id());
+  }).then(async () => {
+    await trash(['bootstrap*', '!bootstrap']);
     copydir.sync('./bootstrap/dist/css', '.');
-
-    packageJson.version = currentVersion;
+    packageJson.version = version.substr(1);
     fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2), 'utf8');
-
-    const registry = 'https://registry.npmjs.org';
-
-    // publish
     npminit.load({
-
     }, (er, npm) => {
       if (er) throw er;
-
       npm.config.setCredentialsByURI(registry, { token: process.env.NPM_TOKEN });
-
-      npm.commands.publish(['.']);
+      npm.commands.publish(['.'], async () => {
+        if (currentPosition < tagLength - 1) {
+          currentPosition++;
+          checkout(repo, versions, tagLength);
+        } else {
+          await trash(['bootstrap*', '!bootstrap']);
+          Object.keys(distTags).map(tag => {
+            npm.commands['dist-tag'](['add', `bootstrap-nojs@${distTags[tag]}`, tag]);
+          });
+        }
+      });
     });
   }).catch(console.error);
-})();
+};
