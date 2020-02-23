bd9d5d328e505b1b0f2ec429f9e15110c7258697
pierreneter <pierre@pierreneter.com>
2020-02-23 13:37:58 +0700

    update code, dependencies

--- index.js
+++ index.js
@@ -11,19 +11,76 @@ const packageJson = require('./package.json');
 
 const registry = 'https://registry.npmjs.org';
 let currentPosition = 0;
-let tagLength = 0;
+let mainTagLength = 0;
 let distTags = [];
 
 (async () => {
-  const originRawRes = await r2('https://registry.npmjs.org/bootstrap').response;
+  const originRawRes = await r2('https://registry.npmjs.org/bootstrap')
+    .response;
   const originRes = await originRawRes.json();
   const rawOriginReleases = originRes.versions;
-  const originReleasesTagName = Object.keys(rawOriginReleases).map(release => `v${release}`);
+  const originReleasesTagName = Object.keys(rawOriginReleases).map(
+    (release) => `v${release}`,
+  );
   distTags = originRes['dist-tags'];
 
   const nojsRes = await r2('https://registry.npmjs.org/bootstrap-nojs').json;
   const rawNojsReleases = nojsRes.versions;
-  const nojsReleasesTagName = Object.keys(rawNojsReleases).map(release => `v${release}`);
+  const nojsReleasesTagName = Object.keys(rawNojsReleases).map(
+    (release) => `v${release}`,
+  );
+
+  const checkout = (repo, versions, tagLength) => {
+    const version = versions[currentPosition];
+    let commit = null;
+    Git.Reference.dwim(repo, `refs/tags/${version}`)
+      .then((ref) => ref.peel(Git.Object.TYPE.COMMIT))
+      .then((ref) => repo.getCommit(ref))
+      .then((rawCommit) => {
+        commit = rawCommit;
+        return Git.Checkout.tree(repo, commit, {
+          checkoutStrategy: Git.Checkout.STRATEGY.SAFE,
+        });
+      })
+      .then(() => repo.setHeadDetached(
+        commit,
+        repo.defaultSignature,
+        `Checkout: HEAD ${commit.id()}`,
+      ))
+      .then(async () => {
+        await trash(['bootstrap*', '!bootstrap']);
+        copydir.sync('./bootstrap/dist/css', '.');
+        packageJson.version = version.substr(1);
+        fs.writeFileSync(
+          './package.json',
+          JSON.stringify(packageJson, null, 2),
+          'utf8',
+        );
+        npminit.load({}, (er, npm) => {
+          if (er) throw er;
+          npm.config.setCredentialsByURI(registry, {
+            token: process.env.NPM_TOKEN,
+          });
+          npm.commands.publish(['.'], async () => {
+            if (currentPosition < tagLength - 1) {
+              currentPosition += 1;
+              checkout(repo, versions, tagLength);
+            } else {
+              await trash(['bootstrap*', '!bootstrap']);
+              Object.keys(distTags).map((tag) => {
+                npm.commands['dist-tag']([
+                  'add',
+                  `bootstrap-nojs@${distTags[tag]}`,
+                  tag,
+                ]);
+                return false;
+              });
+            }
+          });
+        });
+      })
+      .catch(console.error);
+  };
 
   const needRelease = xor(originReleasesTagName, nojsReleasesTagName, [
     'v3.3.7',
@@ -41,51 +98,17 @@ let distTags = [];
     'v4.0.0-alpha.4',
     'v4.0.0-alpha.3',
     'v4.0.0-alpha.2',
-  ]).filter(version => semver.gt(version, '3.3.5'));
-  tagLength = needRelease.length;
+  ]).filter((version) => semver.gt(version, '3.3.5'));
+  mainTagLength = needRelease.length;
 
   if (!needRelease.length) return;
 
-  if (fs.existsSync('./bootstrap')) { await trash(['./bootstrap']); }
+  if (fs.existsSync('./bootstrap')) {
+    await trash(['./bootstrap']);
+  }
   await Git.Clone.clone('https://github.com/twbs/bootstrap', 'bootstrap');
   const repo = await Git.Repository.open(path.resolve(__dirname, 'bootstrap'));
 
-  checkout(repo, needRelease, tagLength);
+  checkout(repo, needRelease, mainTagLength);
   await trash(['bootstrap*', '!bootstrap']);
 })();
-
-const checkout = (repo, versions, tagLength) => {
-  const version = versions[currentPosition];
-  let commit = null;
-  Git.Reference.dwim(repo, `refs/tags/${version}`).then(function (ref) {
-    return ref.peel(Git.Object.TYPE.COMMIT);
-  }).then(function (ref) {
-    return repo.getCommit(ref);
-  }).then((rawCommit) => {
-    commit = rawCommit;
-    return Git.Checkout.tree(repo, commit, { checkoutStrategy: Git.Checkout.STRATEGY.SAFE });
-  }).then(() => {
-    return repo.setHeadDetached(commit, repo.defaultSignature, "Checkout: HEAD " + commit.id());
-  }).then(async () => {
-    await trash(['bootstrap*', '!bootstrap']);
-    copydir.sync('./bootstrap/dist/css', '.');
-    packageJson.version = version.substr(1);
-    fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2), 'utf8');
-    npminit.load({
-    }, (er, npm) => {
-      if (er) throw er;
-      npm.config.setCredentialsByURI(registry, { token: process.env.NPM_TOKEN });
-      npm.commands.publish(['.'], async () => {
-        if (currentPosition < tagLength - 1) {
-          currentPosition++;
-          checkout(repo, versions, tagLength);
-        } else {
-          await trash(['bootstrap*', '!bootstrap']);
-          Object.keys(distTags).map(tag => {
-            npm.commands['dist-tag'](['add', `bootstrap-nojs@${distTags[tag]}`, tag]);
-          });
-        }
-      });
-    });
-  }).catch(console.error);
-};
